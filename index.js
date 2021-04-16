bbbd7d3183769c04f5c59c04dd741bc32ca4e9e8
Mike Rhiney <rhineyman@gmail.com>
2021-04-16 09:48:41 -0700

    table viewable in terminal and quit kills prompt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const connection = require('./config/connection');
-
+// const consTable = require("console.table");
 
 const init = () => {
     inquirer.prompt({
@@ -31,9 +31,30 @@ const init = () => {
 });
 };
 
-const viewEmployees = () => {
+const viewEmployees = () => { 
 
+  var query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);   
+
+    init();
+  });
 };
+
+ const addEmployee = () => {
+
+ }
 
 connection.connect((err) => {
   if (err) throw err;
